01a73b7e3c4a06a516bc0b855863017f30fea5be
jedy <bobbjedy@gmail.com>
2020-06-21 02:13:22 +0300

    bef touch

---
+++ www/client/components/touch-control.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    // let xStart, yStart;
+    // document.getElementById('left-touch').addEventListener('touchstart', e => {
+    //     console.log('start', e.touches);
+    //     xStart = e.touches[0].clientX;
+    //     yStart = e.touches[0].clientY;
+    //     console.log({ xStart, yStart });
+    // });
+
+}, 2000);
